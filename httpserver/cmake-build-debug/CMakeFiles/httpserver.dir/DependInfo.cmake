1e68129940517f9fc09c7c9eb5753accbb26070b
LickyQi <liqiayyz@gmail.com>
2020-03-16 17:24:23 +0800

    Http Server and Client

---
+++ httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/liqi/Desktop/httpserver/connection.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/connection.cpp.o"
+  "/Users/liqi/Desktop/httpserver/connection_manager.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/connection_manager.cpp.o"
+  "/Users/liqi/Desktop/httpserver/main.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/main.cpp.o"
+  "/Users/liqi/Desktop/httpserver/mime_types.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/mime_types.cpp.o"
+  "/Users/liqi/Desktop/httpserver/reply.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/reply.cpp.o"
+  "/Users/liqi/Desktop/httpserver/request_handler.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/request_handler.cpp.o"
+  "/Users/liqi/Desktop/httpserver/request_parser.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/request_parser.cpp.o"
+  "/Users/liqi/Desktop/httpserver/server.cpp" "/Users/liqi/Desktop/httpserver/cmake-build-debug/CMakeFiles/httpserver.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/boost/1.72.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
